b1e2c9fb109f6557352b6a6434faadb9fc303b7c
waterylime46@aol.com <waterylime46@aol.com>
2019-11-14 10:08:00 -0700

    html and rendering html left

--- js/app.js
+++ js/app.js
@@ -1,54 +1,195 @@
-const Employee = require('./Template-Engine/js/Employee.js');
-const Engineer = require('./Template-Engine/js/Engineer.js');
-const Intern = require('./Template-Engine/js/intern.js');
-const Manager = require('./Template-Engine/js/Manager.js');
-const util = require ('util');
-const inquire = ('inquire');
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const generateHTML = require("./generateHTML");
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
-const teamArr = [];
-
-init() =>{
-    inquire
-    prompt([
-        {
-        type:'input'.
-        name: 'manager',
-        message: 'What is your managers name?'
-    },
-    {
-        type:'input'.
-        name: 'id',
-        message: 'What is your employee ID?'
-    },
-    {
-        type:'input'.
-        name: 'email',
-        message: 'What is your Office number?'
-    },
-    {
-        type:'input'.
-        name: 'title',
-        message: 'What is your title?'
-    }
-    ]).then((data) =>{
-        const teamManager = new Manager(data.managerName, data.id, data.email, data.officeNumber, 'Manager')
-    
-    
-    team.push(teamManager);
-    return consloe.log(team);
-    }).then( () =>{
-        consloe.log('test')
-    });
-
-
-
-
-
-
+const writeHTML = function(generateHTML) {
+  writeFileAsync("index.html", generateHTML);
 };
 
+const team = [];
 
+//the main function of the app (and the one that triggers on app launch), this function collects the data on the office manager (the first member to be inputed), it then creates a new Manager instance with the collected information, pushes that instance to the team array, and fires the more function.
 
+const teamMaker = () => {
+  console.log(
+    "Welcome. This app will ask you to input various information about a team, and create an HTML file that will showcase that information. Because we live in a hierarchical society, we will start with the team manager."
+  );
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "managerName"
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id"
+      }
+    ])
+    .then(function(data) {
+      const teamManager = new Manager(
+        data.managerName,
+        data.id,
+        data.email,
+        data.officeNumber,
+        "Manager"
+      );
+      team.push(teamManager);
+      return console.log(team);
+    })
+    .then(function() {
+      more();
+    });
+};
 
-teamMaker()
+//a function that collects information for inputed interns, creates a new Intern instance, and adds them to the team array. Then fires the "more" callback function that allows adding more users.
+
+const newIntern = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id"
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "Which school did you go to?",
+        name: "school"
+      }
+    ])
+    .then(function(res) {
+      const member = new Intern(
+        res.name,
+        res.id,
+        res.email,
+        res.school,
+        "Intern"
+      );
+      team.push(member);
+    })
+    .then(function() {
+      more();
+    });
+};
+
+//a function that collects information for inputed engineers, creates a new Engineer instance, and adds them to the team array. Then fires the "more" callback function that allows adding more users.
+
+const newEngineer = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id"
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+      }
+    ])
+    .then(function(res) {
+      const member = new Engineer(
+        res.name,
+        res.id,
+        res.email,
+        res.github,
+        "Engineer"
+      );
+      team.push(member);
+    })
+    .then(function() {
+      more();
+    });
+};
+
+//a function that fires once all information has been collected. Currently only prints team array, will fire the generateHTML function eventually.
+
+const done = () => {
+  console.log(
+    "Thank you for adding all your team members. With use of magic, we will now present you with a webpage for them! Here are all team members added:"
+  );
+  console.log(team);
+};
+
+//a function that asks the user if they would like to input an additional team member. Depending on response, either fires the addAnother function, or the done function
+
+const more = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to enter another team member?",
+        name: "add",
+        choices: ["yes", "no"]
+      }
+    ])
+    .then(function(res) {
+      if (res.add == "yes") {
+        addAnother();
+      } else done();
+    });
+};
+
+//a function that determines if the next team member to be added is an intern or an engineer, that triggers the appropriate "new" function
+
+const addAnother = () => {
+  console.log(
+    "cool, we will continue. Please enter information for the next team member."
+  );
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Is this team member an engineer or an intern?",
+        name: "job",
+        choices: ["engineer", "intern"]
+      }
+    ])
+    .then(function(res) {
+      if (res.job == "engineer") {
+        newEngineer();
+      } else newIntern();
+    });
+};
+
+teamMaker();
